b1e7954e07d71a1af2ef3c5c32e528a13a521cf1
Mohammad Rafiq <googaabumtum@gmail.com>
2025-02-04 17:30:02 +0800

    feat: add hoarder compose file + startup script

---
+++ public-hoarder/load-hoarder-secrets.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Loading env vars from secrets..."
+
+# Load the OpenAI API key from the secret file and export it as an environment variable
+export NEXTAUTH_SECRET=$(cat /run/secrets/nextauth_secret)
+export MEILI_MASTER_KEY=$(cat /run/secrets/meili_master_key)
+export OPENAI_API_KEY=$(cat /run/secrets/openai_api_key)
+
+# Execute the original command (/init)
+# grepped from `docker inspect --format='{{.Config.Entrypoint}} {{.Config.Cmd}}' ghcr.io/hoarder-app/hoarder:release`
+exec /init
